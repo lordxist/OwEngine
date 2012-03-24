cd873ce9b751a7b8e0c23f645a425abd209fffd9
lordxist <julian.jabs@web.de>
2012-03-24 17:58:21 +0100

    Remove generic layer functionality

--- src/owengine/core/world/GameMap.java
+++ src/owengine/core/world/GameMap.java
@@ -2,38 +2,16 @@ package owengine.core.world;
 
 import java.awt.Graphics;
 import java.awt.Point;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class GameMap {
 
-	public static class Layer {
-	
-		private HashMap<Point, String> tileTypes = new HashMap<Point, String>();
-		private String name;
-		
-		public String getName() {
-			return name;
-		}
-	
-		public String getTileType(Point pos) {
-			return tileTypes.get(pos);
-		}
-	
-		public void setTileType(Point pos, String type) {
-			tileTypes.put(pos, type);
-		}
-	
-	}
-
 	private MapRenderer renderComponent;
 	private Set<Entity> entities = new HashSet<Entity>();
 	private Set<Point> blocked = new HashSet<Point>();
-	private List<Layer> layers = new ArrayList<Layer>();
 	private HashMap<Point, StoryEvent> events = new HashMap<Point, StoryEvent>();
 	private StoryEvent event = StoryEvent.NULL_EVENT;
 	private Set<StoryEvent> mapEvents = new HashSet<StoryEvent>();
@@ -104,27 +82,6 @@ public class GameMap {
 
 	public void block(Point point) {
 		blocked.add(point);
-	}
-
-	public void addLayer(Layer layer) {
-		layers.add(layer);
-	}
-
-	public Layer getLayerByZOrder(int zOrder) {
-		return layers.get(zOrder);
-	}
-
-	public Layer getLayerByName(String name) {
-		for (Layer layer : layers) {
-			if (layer.getName().equals(name)) {
-				return layer;
-			}
-		}
-		return null;
-	}
-
-	public List<Layer> getLayers() {
-		return Collections.unmodifiableList(layers);
 	}
 
 	public void addEvent(Point pos, StoryEvent event) {
