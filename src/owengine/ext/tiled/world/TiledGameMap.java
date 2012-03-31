a785b073cf85a825a20301d0231db51769f2da00
lordxist <julian.jabs@web.de>
2012-03-31 16:21:05 +0200

    Implement TiledGameMap#isBlocked(Point)

--- src/owengine/ext/tiled/world/TiledGameMap.java
+++ src/owengine/ext/tiled/world/TiledGameMap.java
@@ -25,7 +25,9 @@ public class TiledGameMap extends GameMap {
 	public boolean isBlocked(Point position) {
 		for (MapLayer layer : tiledMap.getLayers()) {
 			if (layer instanceof TileLayer
-					&& layer.getProperties().getProperty("blocking", "false").equals(true)) {
+					&& layer.getProperties().getProperty("blocking", "false").equals("true")
+					&& ((TileLayer) layer).getTileAt(position.x, position.y) != null
+					&& ((TileLayer) layer).getTileAt(position.x, position.y).getId() != -1) {
 				return true;
 			}
 		}
